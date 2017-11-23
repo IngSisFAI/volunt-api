6fc39931219387deadc0e805066c13431238c24a
Esteban Ruiz de Galarreta <cabeza180@gmail.com>
2017-11-23 18:29:47 -0300

    hook after DonationResponse

--- common/models/donation-response.js
+++ common/models/donation-response.js
@@ -1,5 +1,15 @@
 'use strict';
 
-module.exports = function(Donationresponse) {
+module.exports = function(DonationResponse) {
 
+  DonationResponse.afterRemote('find',(context,result, next)=>{
+
+    //Lo que queremos hacer aca, es verificar si se pidio que la donacion.
+    // tambien traiga al donador, y la donacion es anonima,
+    // agregarle los datos necesarios para que la vista no tenga que comprobarlo.
+    console.log("@@@@@@@@@@@@@@");
+    console.log(context.args.filter);
+
+    next();
+  })
 };
