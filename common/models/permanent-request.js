dca5ea7dfda81206431d2b50285fa38bbc8f9c08
Unknown <agustina.buccella@fi.uncoma.edu.ar>
2017-11-30 16:28:32 -0300

    onetimerequest mas completo

--- common/models/permanent-request.js
+++ common/models/permanent-request.js
@@ -1,5 +1,27 @@
 'use strict';
 
 module.exports = function(Permanentrequest) {
-
+  // Disable related model relations "product"
+  Permanentrequest.disableRemoteMethodByName(
+    'prototype.__get__product');
+  Permanentrequest.disableRemoteMethodByName(
+    'prototype.__create__product');
+  Permanentrequest.disableRemoteMethodByName(
+    'prototype.__delete__product');
+  Permanentrequest.disableRemoteMethodByName(
+    'prototype.__update__product');
+  Permanentrequest.disableRemoteMethodByName(
+    'prototype.__destroy__product');
+  Permanentrequest.disableRemoteMethodByName(
+    'prototype.__findById__product');
+  Permanentrequest.disableRemoteMethodByName(
+    'prototype.__count__product');
+  Permanentrequest.disableRemoteMethodByName(
+    'prototype.__createById__product');
+  Permanentrequest.disableRemoteMethodByName(
+    'prototype.__deleteById__product');
+  Permanentrequest.disableRemoteMethodByName(
+    'prototype.__updateById__product');
+  Permanentrequest.disableRemoteMethodByName(
+    'prototype.__destroyById__product');
 };
