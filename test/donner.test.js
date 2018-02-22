83c3a5b6b9825608ebaf9eccccc813f7b70ac0f0
Maxi <ariasmaxi89@gmail.com>
2018-02-22 17:55:05 -0300

    Added unit testing cases, fixed some models
    - Added unit testing file for donner
    - Added unit testing file for product
    - Modified donation-response to add a status property
    - Fixed donner model, reputation is now of type number

---
+++ test/donner.test.js
@@ -0,0 +1,83 @@
+'use strict';
+
+// Require the dev-dependencies
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const jsonSchema = require('chai-json-schema');
+const server = require('../server/server');
+const fs = require('fs');
+const should = chai.should();
+const expect = chai.expect();
+
+chai.use(chaiHttp);
+chai.use(jsonSchema);
+
+var donnerSchema = {
+  title: 'Donner request schema v1',
+  type: 'object',
+  required: [
+    'name',
+    'lastName',
+    'phoneNumber',
+    'dni'],
+  properties: {
+    name: {
+      type: 'string',
+    },
+    lastName: {
+      type: 'string',
+    },
+    phoneNumber: {
+      type: 'string',
+    },
+    dni: {
+      type: 'string',
+    },
+    reputation: {
+      type: 'number',
+    },
+  },
+};
+
+describe('Donner', (done) => {
+  describe('/GET api/Donner ', function() {
+    this.timeout(100000);
+    it('it should get all Donners', (done) => {
+      chai.request(server)
+        .get('/api/Donners')
+        .end((err, res) => {
+          res.body.should.be.a('array');
+          for (let i = 0; i < res.body.length; i++)
+            res.body[i].should.be.jsonSchema(donnerSchema);
+          res.should.have.status(200);
+          done();
+        });
+    });
+  });
+
+  describe('/POST api/Donner ', function() {
+    this.timeout(100000);
+    it('it should post one Donner', (done) => {
+      chai.request(server)
+        .post('/api/Donners')
+        .send({
+          name: 'Test',
+          lastName: 'User',
+          phoneNumber: '+549299874563',
+          dni: '11222555',
+          email: 'test@user.com',
+          password: '12345',
+          reputation: 0,
+        }
+        )
+        .end((err, res) => {
+          res.body.should.be.a('object');
+          res.body.should.be.jsonSchema(donnerSchema);
+          res.should.have.status(200);
+          done();
+        });
+    });
+  });
+  ;
+});
+
