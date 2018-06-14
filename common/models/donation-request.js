d4caab2ab3de8f452ebf521ae6cfb688cdd3690a
Unknown <agustina.buccella@fi.uncoma.edu.ar>
2018-06-14 17:55:09 -0300

    permanent request promised

--- common/models/donation-request.js
+++ common/models/donation-request.js
@@ -81,9 +81,9 @@ module.exports = function(DonationRequest) {
             // eso viene definido como true en la propiedad isPermanent
 
             if (ctx.req.body.isPermanent) {
-              // es un pedido permanente con lo que amount y promised deberian ser nulos
+              // es un pedido permanente con lo que amount debe ser nulo
               ctx.req.body.amount = null;
-              ctx.req.body.promised = null;
+              ctx.req.body.promised = 0;
               // covered debe iniciar en 0 y se va sumando a medida que van donando
               ctx.req.body.covered = 0;
 
